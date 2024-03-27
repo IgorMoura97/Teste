d8deb67e503c374d55b7fe4f2eae568029b30f4b
IgorMoura97 <164926600+IgorMoura97@users.noreply.github.com>
2024-03-27 20:52:04 -0300

    Update GreetingControllerTests.java

--- complete/src/test/java/com/example/restservice/GreetingControllerTests.java
+++ complete/src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.restservice;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -27,26 +28,32 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+// Classe de teste para testar o controlador GreetingController
 @SpringBootTest
 @AutoConfigureMockMvc
 public class GreetingControllerTests {
 
 	@Autowired
-	private MockMvc mockMvc;
+	private MockMvc mockMvc; // MockMvc é uma classe fornecida pelo Spring para testar controllers MVC
 
+	// Teste para verificar se a mensagem padrão é retornada quando nenhum parâmetro é fornecido
 	@Test
 	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
-		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, World!"));
+		this.mockMvc.perform(get("/greeting")) // Realiza uma solicitação GET para /greeting
+				.andDo(print()) // Imprime detalhes da solicitação e resposta no console
+				.andExpect(status().isOk()) // Verifica se o status da resposta é OK (200)
+				.andExpect(jsonPath("$.content").value("Hello, World!")); // Verifica se o conteúdo da resposta contém a mensagem esperada
 	}
 
+	// Teste para verificar se uma mensagem personalizada é retornada quando um parâmetro é fornecido
 	@Test
 	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
 
-		this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-				.andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+		this.mockMvc.perform(get("/greeting").param("name", "Spring Community")) // Realiza uma solicitação GET para /greeting com o parâmetro 'name' como 'Spring Community'
+				.andDo(print()) // Imprime detalhes da solicitação e resposta no console
+				.andExpect(status().isOk()) // Verifica se o status da resposta é OK (200)
+				.andExpect(jsonPath("$.content").value("Hello, Spring Community!")); // Verifica se o conteúdo da resposta contém a mensagem personalizada esperada
 	}
 
 }
